1773455da502c170c09d5b2c9b74444b421de898
VG08 <vasugarg208@gmail.com>
2021-07-15 14:52:19 +0530

    Added fun cog and commands work

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,21 +1,38 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+from time import sleep
+from glob import glob
+import asyncio
 
 PREFIX = "+"
 OWNER_IDS = [808309036364595202]
+COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
+class Ready():
+	def __init__(self):
+		for cog in COGS:
+			setattr(self, cog, False)
 
+	def ready_up(self, cog):
+		setattr(self, cog, True)
+		print(f"{cog} cog ready")
+
+	def all_ready(self):
+		return all([getattr(self, cog) for cog in COGS])
 class Bot(BotBase):
 	def __init__(self):
+		
 		self.PREFIX = PREFIX
 		self.ready = False
 		self.guild = None
 		self.scheduler = AsyncIOScheduler()
+		self.bot = Bot
+		self.cogs_ready = Ready()
 
 		super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
 
 	def run(self, version):
 		self.VERSION = version
-
+		self.setup()
 		with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
 			self.TOKEN = tf.read()
 
@@ -30,6 +47,10 @@ class Bot(BotBase):
 
 	async def on_ready(self):
 		if not self.ready:
+			while not self.cogs_ready.all_ready():
+				await asyncio.sleep(0.5)
+
+			
 			self.ready = True
 			self.guild = self.get_guild(860876301921288193)
 			print("bot ready")
@@ -37,7 +58,15 @@ class Bot(BotBase):
 		else:
 			print("bot reconnected")
 
-	async def on_message(self, message):
-		print('message')
+	# async def on_message(self,message):
+	# 	if message.content == "+hello":
+	# 		await message.channel.send('Hey there')
+	def setup(self):
+		for cog in COGS:
+			self.load_extension(f"lib.cogs.{cog}")
+			print(f" {cog} cog loaded")
+
+		print("setup complete")
 
 bot = Bot()
+
---
+++ lib/cogs/fun.py
@@ -0,0 +1,51 @@
+
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+import random
+
+class Fun(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(name="Hi", aliases=['hello'], hidden=True)
+    async def hello(self, ctx):
+        await ctx.send('Hey there!')
+
+    @command(aliases=['8ball'])
+    async def _8ball(self, ctx, *, question):
+        response = ['as i see yes.',
+                'Ask again later.',
+                'Better not tell you now.',
+                'Cannot predict now.',
+                'Concentrate and ask again.',
+                'Don’t count on it.',
+                'It is certain.',
+                'It is decidedly so.',
+                'Most likely.',
+                'My reply is no.',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Outlook good.',
+                'Reply hazy, try again.',
+                'Signs point to yes.',
+                'Very doubtful.',
+                'Without a doubt.']
+        await ctx.send(f'Question:{question}\nAnswer: {random.choice(response)}')
+    @command()
+    async def dice(self, ctx):
+        
+        await ctx.send(f'you rolled a {random.randint(1, 6)}')
+    @command()
+    async def randomno(self, ctx):
+        await ctx.send(f'Your random number is {random.randint(1, 1000000)}')
+
+
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("fun")
+
+
+
+def setup(bot):
+	bot.add_cog(Fun(bot))
