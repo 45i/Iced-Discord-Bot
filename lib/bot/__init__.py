2c7f37a535ed79a111a6610bb596900adddc7bd9
Tindi Brown <tindibrown12@gmail.com>
2021-07-15 22:22:53 +1000

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,8 +1,12 @@
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed
 from discord.ext.commands import Bot as BotBase
+from discord.ext import commands
 
 PREFIX = "+"
 OWNER_IDS = [808309036364595202]
+
 
 class Bot(BotBase):
 	def __init__(self):
@@ -11,7 +15,10 @@ class Bot(BotBase):
 		self.guild = None
 		self.scheduler = AsyncIOScheduler()
 
-		super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+		super().__init__(
+			command_prefix=PREFIX, 
+			owner_ids=OWNER_IDS
+			)
 
 	def run(self, version):
 		self.VERSION = version
@@ -31,8 +38,22 @@ class Bot(BotBase):
 	async def on_ready(self):
 		if not self.ready:
 			self.ready = True
-			self.guild = self.get_guild(860876301921288193)
+			self.guild = self.get_guild(860876301921288193) 
 			print("bot ready")
+
+			channel = self.get_channel(862152877312049194)
+			await channel.send("Now Online!")
+
+			embed = Embed(title="Now Online!", description="Iced is now online.",
+			              colour=0xFF0000, timestamp=datetime.utcnow())
+			fields = [("Iced Bot", "The epic multi-purpose-bot", True),
+			         ("AKA, #chadbot", "Brings you the full package for your server!", True),
+			         ("Share and enjoy Iced", "Devs: Tindi Brown, VGO8, SIR F BOMBS special thanks to Sidhath SK!", False)]
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+			embed.set_author(name="Tindi Brown", icon_url=self.guild.icon_url)
+			await channel.send(embed=embed) 
+			embed.set_footer(text= "This is a footer") 
 
 		else:
 			print("bot reconnected")
@@ -41,3 +62,10 @@ class Bot(BotBase):
 		pass
 
 bot = Bot()
+
+#----after bot dev-----#
+#@client.event
+#async def on_ready():
+#	await client.change_presence(activity=discord.Game("Keeping servers Iced!"))
+#	print("Bot is connected!")
+#client.run("ODYyMzA1Mjk5NjkxMDc3NjUy.YOWaZA.CJquymH5JVFyKSmXCopmFwjVl74")
